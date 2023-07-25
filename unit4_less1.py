56df368ac1584e0cb8846b0bde98899a43c931ae
pronina_d <ket2779@mail.ru>
2023-07-25 13:22:42 +0700

    проверка через перебор с конца строки

--- unit4_less1.py
+++ unit4_less1.py
@@ -1,7 +1,7 @@
 def func(a):
-    if a==a[::-1]:
+    if a==a[::-1]:  
         return True
     else:
         return False
-    
+    # выполняем проверку на палиндром через обратный перебор
 print(func('топот'))
